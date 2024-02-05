a0243cdb8c7a7871574689bfa0c2d50077329857
Matheus Henrique <mathttps@gmail.com>
2024-02-05 18:21:16 -0300

    teste

--- src/controllers/controller.rentals.js
+++ src/controllers/controller.rentals.js
@@ -23,7 +23,6 @@ export async function createRental(req, res) {
             return res.sendStatus(400);
         }
 
-
         const gameExists = await executeQuery('SELECT 1 FROM games WHERE id=$1;', [gameId]);
         if (!gameExists.length) {
             await db.query('ROLLBACK');
@@ -45,7 +44,6 @@ export async function createRental(req, res) {
 
         const originalPrice = daysRented * gameExists[0].pricePerDay;
 
-
         const result = await executeQuery(`
             INSERT INTO rentals
             ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee")
@@ -62,84 +60,101 @@ export async function createRental(req, res) {
     }
 }
 
-
 export async function returnRental(req, res){
-    const { id } = req.params
-    const date = dayjs(Date.now()).format('YYYY-MM-DD')
+    const { id } = req.params;
+    const date = dayjs().format('YYYY-MM-DD');
+
     try {
-        const rent = await db.query(`SELECT * FROM rentals WHERE id=$1`, [id])
+        const rent = await db.query(`SELECT * FROM rentals WHERE id=$1 FOR UPDATE;`, [id]);
+
         if (!rent.rows[0]) {
-            return res.sendStatus(404)
+            return res.sendStatus(404);
         }
+
         if (rent.rows[0].returnDate) {
-            return res.sendStatus(400)
-        }
-        const game = await db.query(`SELECT * FROM games WHERE id=$1`, [rent.rows[0].gameId])
-
-        if (dayjs(date).diff(rent.rows[0].rentDate, 'day') >= rent.rows[0].daysRented) {
-            const delay = (dayjs(date).diff(rent.rows[0].rentDate, 'day') - rent.rows[0].daysRented)
-            const value = delay * game.rows[0].pricePerDay
-            await db.query(`UPDATE rentals SET "returnDate"=$1, "delayFee"=$2 WHERE id=$3`, [date, value, id])
-            return res.sendStatus(200)
+            return res.sendStatus(400);
         }
 
-        await db.query(`UPDATE rentals SET "returnDate"=$1, "delayFee"=0 WHERE id=$2`, [date, id])
+        const game = await db.query(`SELECT * FROM games WHERE id=$1;`, [rent.rows[0].gameId]);
 
-        res.sendStatus(200)
+        const delayDays = dayjs(date).diff(rent.rows[0].rentDate, 'day');
+        const delayFee = Math.max(0, delayDays - rent.rows[0].daysRented) * game.rows[0].pricePerDay;
+
+        await db.query(`
+            UPDATE rentals
+            SET "returnDate"=$1, "delayFee"=$2
+            WHERE id=$3;
+        `, [date, delayFee, id]);
+
+        res.sendStatus(200);
     } catch (err) {
-        res.status(500).send(err.message)
+        res.status(500).send(err.message);
     }
 }
 
 export async function deleteRental(req, res){
-    const { id } = req.params
+    const { id } = req.params;
 
     try {
-        const rent = await db.query(`SELECT * FROM rentals WHERE id=$1`, [id])
+        const rent = await db.query(`SELECT * FROM rentals WHERE id=$1`, [id]);
         if (!rent.rows[0]) {
-            return res.sendStatus(404)
+            return res.sendStatus(404);
         }
         if (!rent.rows[0].returnDate) {
-            return res.sendStatus(400)
+            return res.sendStatus(400);
         }
 
-        await db.query(`DELETE FROM rentals WHERE id=$1`, [id])
+        await db.query(`DELETE FROM rentals WHERE id=$1`, [id]);
 
-        res.sendStatus(200)
+        res.sendStatus(200);
     } catch (err) {
-        res.status(500).send(err.message)
+        res.status(500).send(err.message);
     }
 }
 
-export async function getRental(req,res){
+export async function getRental(req, res){
     try {
-        const rentals = await db.query(`SELECT rentals.*, customers.name AS customer_name, games.name AS game_name FROM
-            rentals JOIN customers ON customers.id = "customerId"
-            JOIN games ON games.id = "gameId";`
-        )
+        const rentals = await db.query(`
+            SELECT
+                rentals.id,
+                rentals."customerId",
+                rentals."gameId",
+                rentals."rentDate",
+                rentals."daysRented",
+                rentals."returnDate",
+                rentals."originalPrice",
+                rentals."delayFee",
+                customers.id AS "customer.id",
+                customers.name AS "customer.name",
+                games.id AS "game.id",
+                games.name AS "game.name"
+            FROM
+                rentals
+                JOIN customers ON customers.id = rentals."customerId"
+                JOIN games ON games.id = rentals."gameId";
+        `);
 
-        const list = rentals.rows.map((data)=>(
-            {
-                id: data.id,
-                customerId: data.customerId,
-                gameId: data.gameId,
-                rentDate: dayjs(data.rentDate).format('YYYY-MM-DD'),
-                daysRented: data.daysRented,
-                returnDate: data.returnDate,
-                originalPrice: data.originalPrice,
-                delayFee: data.delayFee,
-                customer:{
-                    id: data.customerId,
-                    name: data.customer_name
-                },
-                game:{
-                    id: data.gameId,
-                    name: data.game_name
-                }
+        const list = rentals.rows.map((data) => ({
+            id: data.id,
+            customerId: data.customerId,
+            gameId: data.gameId,
+            rentDate: dayjs(data.rentDate).format('YYYY-MM-DD'),
+            daysRented: data.daysRented,
+            returnDate: data.returnDate,
+            originalPrice: data.originalPrice,
+            delayFee: data.delayFee,
+            customer: {
+                id: data.customer.id,
+                name: data.customer.name
+            },
+            game: {
+                id: data.game.id,
+                name: data.game.name
             }
-        ))
-        res.send(list)
+        }));
+
+        res.send(list);
     } catch (err) {
-        res.status(500).send(err.message)
+        res.status(500).send(err.message);
     }
 }
